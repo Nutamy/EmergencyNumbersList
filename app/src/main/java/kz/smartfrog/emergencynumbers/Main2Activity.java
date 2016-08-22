8e4ea03a9ea356bf295a3e6cf17e8779f94a0f9e
Nutamy <natasha.pechenka@gmail.com>
2016-08-22 16:20:02 +0600

    pictures include version

--- app/src/main/java/kz/smartfrog/emergencynumbers/Main2Activity.java
+++ app/src/main/java/kz/smartfrog/emergencynumbers/Main2Activity.java
@@ -1,13 +1,25 @@
 package kz.smartfrog.emergencynumbers;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.backendless.Backendless;
+import com.backendless.BackendlessCollection;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.BackendlessDataQuery;
+import com.backendless.persistence.QueryOptions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,42 +27,174 @@ import java.util.List;
 public class Main2Activity extends AppCompatActivity {
 
     private ListView listViewEmergencyPhones;
+    ManualEmergencyPhones emergencyPhones;
     Intent intentToMainAct;
     Button back;
-   // SearchView searchView;
+    Button buttonUpdate;
+    String savedBasePhones = "";
+
+    List<String> textlist = new ArrayList<>();
+    List<ManualEmergencyPhones> listCompanyNames;
+    List<ManualEmergencyPhones> companiesList;
+
+    // SearchView searchView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        buttonUpdate = (Button)findViewById(R.id.buttonUpdate);
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPref.edit();
+        final String savedBaseNumbersReturn = sharedPref.getString("key", "");
+        String [] sarray;
+        Log.d("TAG", "here");
+        listViewEmergencyPhones = (ListView) findViewById(R.id.listViewEmergencyPhones);
+
+        sarray = savedBaseNumbersReturn.split("\n");
+        final ArrayList<String> sarraylist = new ArrayList<>();
+        for (int i = 0; i < sarray.length-1; i++) {
+            sarraylist.add(sarray[i]);
+            Log.d("TAG", "here again");
+            Log.d("TAG", sarraylist.get(i));
+
+        }
+        //String whereClause = "city =  'Almaty'";
+        try {
+            if (sarraylist.get(0) != null) {
+
+                //dataQuery.setWhereClause(whereClause);
+
+
+                listViewEmergencyPhones = (ListView) findViewById(R.id.listViewEmergencyPhones);
+
+                ArrayAdapter<String> adapterOrganisationPhones = new ArrayAdapter<>(Main2Activity.this, R.layout.listview, R.id.textViewInListView, sarraylist);
+
+                listViewEmergencyPhones.setAdapter(adapterOrganisationPhones);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {public void onClick(View v) {
+            textlist.clear();
+
+            String whereClause = "city = 'Almaty'";
+            BackendlessDataQuery dataQuery = new BackendlessDataQuery( whereClause );
+            QueryOptions queryOptions = new QueryOptions(15,0);
+            queryOptions.setRelationsDepth( 1 );
+            dataQuery.setQueryOptions( queryOptions );
+
+//            BackendlessCollection<ManualEmergencyPhones> friends = Backendless.Data.of(ManualEmergencyPhones.class ).find( dataQuery, new AsyncCallback<BackendlessCollection<ManualEmergencyPhones>> );
+//
+//            for( ManualEmergencyPhones friend : friends.getData() )
+//            {
+//
+//            }
+
+
+
+
+
+            Backendless.Persistence.of(ManualEmergencyPhones.class).find(dataQuery, new AsyncCallback<BackendlessCollection<ManualEmergencyPhones>>() {
+
+                @Override
+                public void handleResponse(BackendlessCollection<ManualEmergencyPhones> foundContacts) {
+
+                    companiesList = foundContacts.getData();
+
+                    //String text = "";
+
+
+                    for (int i = 0; i < companiesList.size(); i++) {
+                        textlist.add(companiesList.get(i).getNameOfCompanyRU() + " " + companiesList.get(i).getPhoneNumber() + "\n");
+
+                    }
+
+                    //  listViewEmergencyPhones.getAdapter().
+
+                    ArrayAdapter<String> adapterOrganisationPhones = new ArrayAdapter<>(Main2Activity.this, R.layout.listview, R.id.textViewInListView, textlist);
+
+                    listViewEmergencyPhones.setAdapter(adapterOrganisationPhones);
+
+
+                    for (int i = 0; i < textlist.size(); i++) {
+                        savedBasePhones += textlist.get(i);
+                        Log.d("TAG", savedBasePhones);
+                    }
+                    editor.putString("key", savedBasePhones);
+                    editor.commit();
+                }   // all Contact instances have been found
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+                    String errorret = fault.getCode() + " " + fault.getDetail() + " " + fault.getMessage();// an error has occurred, the error code can be retrieved with fault.getCode()
+
+
+                }
+            });
+
+        }
+        });
+
+
 
         intentToMainAct = new Intent(this, MainActivity.class);
         back = (Button)findViewById(R.id.back);
-     //   searchView = (SearchView)findViewById(R.id.searchView);
-        listViewEmergencyPhones = (ListView)findViewById(R.id.listViewEmergencyPhones);
+        //   searchView = (SearchView)findViewById(R.id.searchView);
 
 
-        List<String> organisationPhones = loadListEmergencyPhones();
-        ArrayAdapter<String> adapterOrganisationPhones = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, organisationPhones);
 
-        listViewEmergencyPhones.setAdapter(adapterOrganisationPhones);
+        // List<String> organisationPhones = loadListEmergencyPhones();
+
 
         back.setOnClickListener(new View.OnClickListener() {public void onClick(View v) {
             startActivity(intentToMainAct);
         }
         });
+
+        // ListView Item Click Listener
+        /*listViewEmergencyPhones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemPosition = position;
+
+                // ListView Clicked item value
+                String  itemValue = (String) listViewEmergencyPhones.getItemAtPosition(position);
+
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
+                        .show();
+
+            }
+
+        });*/
+
+
+
+
     }
 
-    private List<String> loadListEmergencyPhones() {
-        List<String> list = new ArrayList<String>();
-        for (int i = 0; i <20 ; i++) {
-            list.add("Телефон доверия  "+"  115");
-            list.add("Центр обслуживания населения  "+"  1414");
-            list.add("Телефон доверия бытового насилия (для женщин)  "+ "  1415");
-            list.add("Защита прав потребителя  "+"  2040");
-        }
+//    private List<String> loadListEmergencyPhones() {
+//        final List<ManualEmergencyPhones> list = new ArrayList<ManualEmergencyPhones>();
+//
+//
+////        for (int i = 0; i <20 ; i++) {
+////            list.add("Телефон доверия  "+"  115");
+////            list.add("Центр обслуживания населения  "+"  1414");
+////            list.add("Телефон доверия бытового насилия (для женщин)  "+ "  1415");
+////            list.add("Защита прав потребителя  "+"  2040");
+////        }
+//
+//
+//        return list;
+//    }
 
 
-        return list;
-    }
 }
--- app/src/main/java/kz/smartfrog/emergencynumbers/MainActivity.java
+++ app/src/main/java/kz/smartfrog/emergencynumbers/MainActivity.java
@@ -1,53 +1,202 @@
 package kz.smartfrog.emergencynumbers;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.backendless.Backendless;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.playlog.internal.LogEvent;
 
 public class MainActivity extends AppCompatActivity {
     Intent intentToManualEmergencyPhones;
-    Button exNumber101;
-    Button exNumber102;
-    Button exNumber103;
-    Button exNumber104;
-    Button exNumber112;
+    Intent intentCall;
+    ImageView imageView101;
+    ImageView imageView102;
+    ImageView imageView103;
+    ImageView imageView104;
+    ImageView imageView112;
+    ImageButton exNumber101;
+    ImageButton exNumber102;
+    ImageButton exNumber103;
+    ImageButton exNumber104;
+    ImageButton exNumber112;
     Button manualEmergencyPhones;
     TextView textView101;
     TextView textView102;
     TextView textView103;
     TextView textView104;
     TextView textView112;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String appVersion = "v1";
-        Backendless.initApp( this, "11429BC2-042A-4E59-FF22-4F9248E4DF00", "1D3DF352-7502-A80D-FF37-2C6146278A00", appVersion );
+        Backendless.initApp(this, "11429BC2-042A-4E59-FF22-4F9248E4DF00", "1D3DF352-7502-A80D-FF37-2C6146278A00", appVersion);
 
-        exNumber101 = (Button)findViewById(R.id.button101);
-        exNumber102 = (Button)findViewById(R.id.button102);
-        exNumber103 = (Button)findViewById(R.id.button103);
-        exNumber104 = (Button)findViewById(R.id.button104);
-        exNumber112 = (Button)findViewById(R.id.button112);
-        manualEmergencyPhones = (Button)findViewById(R.id.buttonManualEmergPhones);
-        textView101 = (TextView)findViewById(R.id.textView101);
-        textView102 = (TextView)findViewById(R.id.textView102);
-        textView103 = (TextView)findViewById(R.id.textView103);
-        textView104 = (TextView)findViewById(R.id.textView104);
-        textView112 = (TextView)findViewById(R.id.textView112);
+        exNumber101 = (ImageButton) findViewById(R.id.imageButton101);
+        exNumber102 = (ImageButton) findViewById(R.id.imageButton102);
+        exNumber103 = (ImageButton) findViewById(R.id.imageButton103);
+        exNumber104 = (ImageButton) findViewById(R.id.imageButton104);
+        exNumber112 = (ImageButton) findViewById(R.id.imageButton112);
+        manualEmergencyPhones = (Button) findViewById(R.id.buttonManualEmergPhones);
+        imageView101 = (ImageView)findViewById(R.id.imageView101);
+        imageView102 = (ImageView)findViewById(R.id.imageView102);
+        imageView103 = (ImageView)findViewById(R.id.imageView103);
+        imageView104 = (ImageView)findViewById(R.id.imageView104);
+        imageView112 = (ImageView)findViewById(R.id.imageView112);
+        textView101 = (TextView) findViewById(R.id.textView101);
+        textView102 = (TextView) findViewById(R.id.textView102);
+        textView103 = (TextView) findViewById(R.id.textView103);
+        textView104 = (TextView) findViewById(R.id.textView104);
+        textView112 = (TextView) findViewById(R.id.textView112);
         intentToManualEmergencyPhones = new Intent(this, Main2Activity.class);
 
-        manualEmergencyPhones.setOnClickListener(new View.OnClickListener() {public void onClick(View v) {
-            startActivity(intentToManualEmergencyPhones);
-        }
+        imageView101.setClickable(true);
+        imageView102.setClickable(true);
+        imageView103.setClickable(true);
+        imageView104.setClickable(true);
+        imageView112.setClickable(true);
+
+        manualEmergencyPhones.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intentToManualEmergencyPhones);
+            }
         });
 
+
+        View.OnClickListener call101 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentCall = new Intent(Intent.ACTION_DIAL);
+                intentCall.setData(Uri.parse("tel:101"));
+
+                Log.d("TAG", intentCall.getData().toString());
+                startActivity(intentCall);
+                Log.d("TAG", "call 101");
+            }
+        };
+
+        exNumber101.setOnClickListener(call101);
+        imageView101.setOnClickListener(call101);
+
+        View.OnClickListener call102 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentCall = new Intent(Intent.ACTION_DIAL);
+                intentCall.setData(Uri.parse("tel:102"));
+
+                Log.d("TAG", intentCall.getData().toString());
+                startActivity(intentCall);
+                Log.d("TAG", "call 102");
+            }
+        };
+
+        exNumber102.setOnClickListener(call102);
+        imageView102.setOnClickListener(call102);
+
+        View.OnClickListener call103 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentCall = new Intent(Intent.ACTION_DIAL);
+                intentCall.setData(Uri.parse("tel:103"));
+
+                Log.d("TAG", intentCall.getData().toString());
+                startActivity(intentCall);
+                Log.d("TAG", "call 103");
+            }
+        };
+
+        exNumber103.setOnClickListener(call103);
+        imageView103.setOnClickListener(call103);
+
+
+        View.OnClickListener call104 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentCall = new Intent(Intent.ACTION_DIAL);
+                intentCall.setData(Uri.parse("tel:104"));
+
+                Log.d("TAG", intentCall.getData().toString());
+                startActivity(intentCall);
+                Log.d("TAG", "call 104");
+            }
+        };
+
+        exNumber104.setOnClickListener(call104);
+        imageView104.setOnClickListener(call104);
+
+        View.OnClickListener call112 = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentCall = new Intent(Intent.ACTION_DIAL);
+                intentCall.setData(Uri.parse("tel:112"));
+                Log.d("TAG", intentCall.getData().toString());
+                startActivity(intentCall);
+                Log.d("TAG", "call 112");
+            }
+        };
+
+        exNumber112.setOnClickListener(call112);
+        imageView112.setOnClickListener(call112);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://kz.smartfrog.emergencynumbers/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://kz.smartfrog.emergencynumbers/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
